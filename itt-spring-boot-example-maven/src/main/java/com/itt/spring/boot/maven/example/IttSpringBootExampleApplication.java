4133b8cb1607aaab4f4e899be2a739d4ef484a60
ashish-dadhich <Ashish.dadhich@intimetec.com>
2018-04-12 15:08:29 +0530

    initial commit for maven project

---
+++ itt-spring-boot-example-maven/src/main/java/com/itt/spring/boot/maven/example/IttSpringBootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.itt.spring.boot.maven.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IttSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IttSpringBootExampleApplication.class, args);
+	}
+}
---
+++ itt-spring-boot-example-maven/src/test/java/com/itt/spring/boot/maven/example/IttSpringBootExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itt.spring.boot.maven.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IttSpringBootExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
