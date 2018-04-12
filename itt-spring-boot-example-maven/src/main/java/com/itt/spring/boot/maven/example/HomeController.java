1f8a344edc16e86ab7ead496588798a739e28dee
ashish-dadhich <Ashish.dadhich@intimetec.com>
2018-04-12 16:55:51 +0530

    Adding type of request, like get/post etc

--- itt-spring-boot-example-maven/src/main/java/com/itt/spring/boot/maven/example/HomeController.java
+++ itt-spring-boot-example-maven/src/main/java/com/itt/spring/boot/maven/example/HomeController.java
@@ -1,12 +1,13 @@
 package com.itt.spring.boot.maven.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-	@RequestMapping("/home")
+	@RequestMapping(value = "/home", method = {RequestMethod.GET})
 	public String hello() {
 		return "Hello hello bol ke, tere aaju baju dol ke !!!";
 	}
